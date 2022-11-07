9b7dd43fec9019fa4b50f831784a4a1a07bc81db
irfanasri <irfan96.asri@gmail.com>
2022-11-07 20:58:35 +0800

    Create models

---
+++ app/Models/Task.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'descriptions',
+        'status',
+        'project_id',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function project()
+    {
+        return $this->hasOne(Project::class);
+    }
+}
